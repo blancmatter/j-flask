c319c9575682373dcd5e0d532c625f8a21216388
Doug Arnold <doug.arnold@cgi.com>
2022-10-10 14:06:12 +0100

    First Commit

---
+++ cafe.py
@@ -0,0 +1,16 @@
+
+from random import random
+
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+from functions import hello, bill
+
+hello('Please pay your bill', 'Julie\n')
+julies_bill = bill()
+
+print('Julies bill is £', julies_bill)
+
+
+
+
---
+++ functions.py
@@ -0,0 +1,8 @@
+import random
+
+def hello(message, name):
+    # Prints a hello message
+    print('Hello', name, message)
+
+def bill():
+    return random.randint(0,9)
